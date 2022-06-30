731c1b4a6c5f72eabdbcf9369abddf778911f27d
Ibrahim Bashir Ilyasu <ibrahimbashirilyasu@gmail.com>
2022-06-30 14:24:08 -0700

    Alx Low Level Language

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+/**                                                * main - Entry point
+ * Description: get "\ "and that piece of art is useful" - Dora Korpar, 2015-10-19\n"*
+ * Return: Always 1 (Success)                      */
+int main(void)
+{
+	gets("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	return 1;
+}
