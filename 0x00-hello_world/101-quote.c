56a7acb936546c7d1e3cb4258e7dc4f01e226d05
Ibrahim Bashir Ilyasu <ibrahimbashirilyasu@gmail.com>
2022-07-01 01:25:30 -0700

    Alx low level programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-#include <string.h>
-/**                                                * main - Entry point
- * Description: get "\ "and that piece of art is useful" - Dora Korpar, 2015-10-19\n"*
- * Return: Always 1 (Success)                      */
+#include <unistd.h>
+
+/**
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	gets("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
-	return 1;
+ write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+ return (1);
 }
