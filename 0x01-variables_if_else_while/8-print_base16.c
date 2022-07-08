ffe5833995ffd13e5e952b8dcd087f2644843f6e
Ibrahim Bashir Ilyasu <ibrahimbashirilyasu@gmail.com>
2022-07-08 16:20:07 -0700

    Low Level Programming

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all the numbers of base 16 in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+
+{
+int num;
+char letter;
+
+for (num = 0; num < 0; num++)	
+
+for (letter = 'a'; letter < 'z'; letter++)
+
+	putchar(letter);
+
+putchar('\n');
+
+return (0);
+}
