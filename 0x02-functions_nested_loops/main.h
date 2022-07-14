1326b77c92cf68ca243812cb417a3547d66b40fa
Ibrahim Bashir Ilyasu <ibrahimbashirilyasu@gmail.com>
2022-07-14 00:31:23 -0700

    Low Level Programming

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,30 +1,18 @@
-
-int _putchar(char k); 
+#ifndef MAIN_H 
+ #define MAIN_H 
   
+ int _putchar(char); 
  void print_alphabet(void); 
-  
  void print_alphabet_x10(void); 
-  
  int _islower(int c); 
-  
  int _isalpha(int c); 
-  
  int print_sign(int n); 
-  
- int _abs(int a); 
-  
- int print_last_digit(int a); 
-  
+ int _abs(int); 
+ int print_last_digit(int); 
  void jack_bauer(void); 
-  
  void times_table(void); 
-  
  int add(int a, int b); 
+ void print_to_98(int n); 
+ void print_times_table(int n); 
   
- void print_to_98(int n);
-
- int add(int one, int two);
- 
- void print_times_table(int n);
-
-
+ #endif
